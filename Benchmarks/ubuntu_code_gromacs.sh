b542ecf72113f1ae5db67428fae5aac944c8d362
Taner Karagöl <149885347+karagol-taner@users.noreply.github.com>
2024-12-05 11:43:59 +0300

    Delete Benchmarks directory

--- Benchmarks/ubuntu_code_gromacs.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update     # This updates the package lists
-sudo apt upgrade    # This upgrades the installed packages to their latest versions
-
-sudo apt install gromacs   # This installs gromacs
-
-wget <URL>   # For downloading benchmark file
-
-gmx mdrun -s benchMEM.tpr -nsteps 5000 -deffnm benchMEM && tar cvzf benchMEM.tar benchMEM*    # For benchmark
